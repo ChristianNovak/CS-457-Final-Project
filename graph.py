a14bbb1d23ce386a3d39df394812b3999b670323
Christian <cnovak487@gmail.com>
2021-12-11 16:37:16 -0500

    Implemented airgraph-ng

---
+++ graph.py
@@ -0,0 +1,34 @@
+import sys
+import os
+
+def graphMenu():
+	print("1. Create a Client to Access Point Relationship Graph")
+	print("2. Create a Client to Probe Request Graph"0
+	
+def graphInput():
+	graphMenu()
+	command = input("> ")
+	if command == "1":
+		clientAPGraph()
+	elif command == "2":
+		clientProbeGraph()
+	else:
+		return
+
+def clientAPGraph():
+	print("Please specify the source CSV file:")
+	csv = input("> ")
+	print("Please specify the output PNG file:")
+	png = input("> ")
+	
+	cmd = "airgraph-ng -i " + csv + " -o " + png + " -g CAPR"
+	os.system(cmd)
+	
+def clientProbeGraph():
+	print("Please specify the source CSV file:")
+	csv = input("> ")
+	print("Please specify the output PNG file:")
+	png = input("> ")
+	
+	cmd = "airgraph-ng -i " + csv + " -o " + png + " -g CPG"
+	os.system(cmd)
