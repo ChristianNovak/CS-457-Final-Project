4a350acb31a2a04cdab5236f265ff83a5d89bf08
Joe Maples <joe@maples.dev>
2021-12-12 16:27:43 -0500

    Implement 2 more WEP Cracks

--- crack.py
+++ crack.py
@@ -3,12 +3,18 @@ import os
 import configparser
 
 def crackMenu():
-	print("1. Crack a WEP/WPA/WPA2 key")
+	print("1. Crack a WEP/WPA/WPA2 key with a wordlist")
+	print("2. Crack a WEP key with IVs")
+	print("3. Crack a WEP key with IVs using KoreK method")
 	
 def crackInput():
 	crackMenu()
 	if command == "1":
 		keyCrack(config)
+	elif command == "2":
+			wepCrack(config)
+	elif command == "3":
+			wepCrackKoreK(config)
 	else:
 		return
 
@@ -19,3 +25,15 @@ def keyCrack(config):
 	capFile = input("Please specify a capture file: ")
 	cmd = "sudo aircrack-ng -w " + wordList + " -b " + bssid + " " + capFile
 	os.system(cmd)
+
+def wepCrack(config):
+    #Aircrack command to crack the key in the traffic dump
+    	bssid = config['ap']['bssid']
+    	capFile = input("Please specify a capture file: ")
+    	cmd = "sudo aircrack-ng -b " + bssid + " " + capFile
+
+def wepCrackKoreK(config):
+    #Aircrack command to crack the key in the traffic dump
+    	bssid = config['ap']['bssid']
+    	capFile = input("Please specify a capture file: ")
+		cmd = "sudo aircrack-ng -K -b " + bssid + " " + capFile
